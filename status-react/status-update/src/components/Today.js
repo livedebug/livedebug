45b38e2f0b10e1d7069b6b3004f89217ece7789f
Matthew Tippett <tippettm@gmail.com>
2020-07-28 23:18:52 -0700

    Update test target.json, display on first 4 displayable.

--- status-react/status-update/src/components/Today.js
+++ status-react/status-update/src/components/Today.js
@@ -8,46 +8,50 @@ function Today(props) {
 
   const [data, setData] = useState({ tasks: [    ]});
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
-        console.log("fetching");
-      const result = await axios(
-        `target.json`,
+       const result = await axios(
+        `/target.json`,
       );
       setData(result.data);
     };
 
     setInterval(fetchData, 5000);
-
     fetchData();
   }, []);
 
+   let first_four = [];
+  // Subset target with the first 4 to be displayed.
+  for (const task of data.tasks) {
+    if(task.display) {
+      let count = first_four.push(task);
+      if (count >= 4) {
+        break;
+      }
+    }
+  }
 
-    console.log("data is ", JSON.stringify(data));
-
-      return <div className="content pure-u-1 pure-u-md-3-4">
-        <div>
-          <div className="post-meta">
-            Targets for today's Twitch...
+  return <div className="content pure-u-1 pure-u-md-3-4">
+      <div>
+        <div className="post-meta">
+          Targets for today's Twitch...
           </div>
           <div className="post-description">
-              <ul>
-              { console.log("x",JSON.stringify(data.tasks))}
-
-                { data.tasks.map(task => (
+            <ul>
+              { first_four.map(task => (
                 <li key={task.desc}>
                 {task.complete
                   ? <strike>{task.desc}</strike>
                   : <span>{task.desc }</span>
                 }
                 </li>
-              ))
+                ))
               }
-              </ul>
+            </ul>
           </div>
         </div>
       </div>;
-    }
+}
 
 
 
